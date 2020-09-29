1270403f38262d012df455a1a12a1b73ac637539
decade <15757160454@163.com>
2020-09-29 11:36:44 +0800

    修改注释

--- src/main/java/com/qxkj/generator/dubbo/DubboStub.java
+++ src/main/java/com/qxkj/generator/dubbo/DubboStub.java
@@ -7,9 +7,12 @@ import java.lang.annotation.Target;
 
 /**
  * Copyright  2020年 dubbo-stub-generator. All rights reserved.
- * <p>
- * TODO: 为了共建和谐社会，请为该类写点注释吧...
- * <p>
+ *
+ * 用于在java编译时自动生成Dubbo Stub类
+ *  Stub类的功能是：
+ *    将Spring Security领域中的用户信息传到Dubbo的RPC上下文中，
+ *    以便RPC调用时客户端和服务端能以同一认证用户进行业务处理，并且无感于业务层
+ *
  * version 1.0.0
  *
  * @author decade
